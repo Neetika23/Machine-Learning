1159c67e0ee11957a02d6279c7593bc522031562
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-21 15:23:09 +0530

    Update pca.py

--- Preprocessing/Selecting Features/pca.py
+++ Preprocessing/Selecting Features/pca.py
@@ -9,3 +9,12 @@ transformed_X = pca.fit_transform(wine_X)
 
 # Look at the percentage of variance explained by the different components
 print(pca.explained_variance_ratio_)
+
+# Split the transformed X and the y labels into training and test sets
+X_wine_train, X_wine_test, y_wine_train, y_wine_test = train_test_split(transformed_X,y)
+
+# Fit knn to the training data
+knn.fit(X_wine_train,y_wine_train)
+
+# Score knn on the test data and print it out
+knn.score(X_wine_test,y_wine_test)
