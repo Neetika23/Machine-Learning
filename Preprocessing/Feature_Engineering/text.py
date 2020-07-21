1e3489d3491328acf903b934da19be88b786fd38
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-21 13:31:25 +0530

    Update text.py

--- Preprocessing/Feature_Engineering/text.py
+++ Preprocessing/Feature_Engineering/text.py
@@ -6,3 +6,14 @@ tfidf_vec = TfidfVectorizer()
 
 # Transform the text into tf-idf vectors
 text_tfidf = tfidf_vec.fit_transform(title_text)
+
+
+# Split the dataset according to the class distribution of category_desc
+y = volunteer["category_desc"]
+X_train, X_test, y_train, y_test = train_test_split(text_tfidf.toarray(), y, stratify=y)
+
+# Fit the model to the training data
+nb.fit(X_train, y_train)
+
+# Print out the model's accuracy
+print(nb.score(X_test, y_test))
