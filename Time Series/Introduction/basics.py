20613a55b2f9193aa38c11f9fb565d028a06fc9a
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-22 12:12:52 +0530

    Create basics.py

---
+++ Time Series/Introduction/basics.py
@@ -0,0 +1,26 @@
+# Print the first 5 rows for inspection
+print(data.head())
+
+from sklearn.svm import LinearSVC
+
+# Construct data for the model
+X = data[['petal length (cm)', 'petal width (cm)']]
+y = data[['target']]
+
+# Fit the model
+model = LinearSVC()
+model.fit(X, y)
+
+# Create input array
+X_predict = targets[['petal length (cm)', 'petal width (cm)']]
+
+# Predict with the model
+predictions = model.predict(X_predict)
+print(predictions)
+
+# Visualize predictions and actual values
+plt.scatter(X_predict['petal length (cm)'], X_predict['petal width (cm)'],
+            c=predictions, cmap=plt.cm.coolwarm)
+plt.title("Predicted class values")
+plt.show()
+
