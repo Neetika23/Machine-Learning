b0c460c5be692ad4afeeb9ec68bfba686c5ba845
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-23 17:09:58 +0530

    Create visualize.py

---
+++ Time Series/Prediction over time/visualize.py
@@ -0,0 +1,13 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+# Split our data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                    train_size=.8, shuffle=False, random_state=1)
+
+# Fit our model and generate predictions
+model = Ridge()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+score = r2_score(y_test, predictions)
+print(score)
