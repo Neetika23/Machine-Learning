14a6f9e32f263f8cb7a20460760737fba43cad46
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-08-07 17:53:47 +0530

    Create binning.py

---
+++ Feature Engineering/Creating Features/binning.py
@@ -0,0 +1,15 @@
+# Import numpy
+import numpy as np
+
+# Specify the boundaries of the bins
+bins = [-np.inf, 10000, 50000, 100000, 150000, np.inf]
+
+# Bin labels
+labels = ['Very low', 'Low', 'Medium', 'High', 'Very high']
+
+# Bin the continuous variable ConvertedSalary using these boundaries
+so_survey_df['boundary_binned'] = pd.cut(so_survey_df['ConvertedSalary'], 
+                                         bins, labels = ['Very low', 'Low', 'Medium', 'High', 'Very high'])
+
+# Print the first 5 rows of the boundary_binned column
+print(so_survey_df[['boundary_binned', 'ConvertedSalary']].head())
