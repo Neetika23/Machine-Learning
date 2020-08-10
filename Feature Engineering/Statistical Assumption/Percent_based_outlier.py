7a428d39357cc4164ff7da94d63493dfaac4f264
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-08-10 19:52:17 +0530

    Create Percent_based_outlier.py

---
+++ Feature Engineering/Statistical Assumption/Percent_based_outlier.py
@@ -0,0 +1,14 @@
+# Find the 95th quantile
+quantile = so_numeric_df['ConvertedSalary'].quantile(0.95)
+
+# Trim the outliers
+trimmed_df = so_numeric_df[so_numeric_df['ConvertedSalary'] < quantile]
+
+# The original histogram
+so_numeric_df[['ConvertedSalary']].hist()
+plt.show()
+plt.clf()
+
+# The trimmed histogram
+trimmed_df[['ConvertedSalary']].hist()
+plt.show()
